0f1013d9fc31da4015d0c9021f1ccb21c335d0dc
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-08-05 16:23:34 +0530

    Add typecasting examples in Python
    Added two files in the 04_typecasting folder:
    - 01_typecasting.py: Demonstrates different typecasting methods in Python (int, float, str, etc.)
    - example.py: Contains additional typecasting examples and practical usage
    
    These files help illustrate how to convert between data types in Python with sample code.

---
+++ Python/04_typecasting/01_typecasting.py
@@ -0,0 +1,17 @@
+a = 34
+b = "34"
+d= 334
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+c = int(b)
+print(c)
+print(type(c))
+
+e = str(d)
+print(d)
+print(type(e))
---
+++ Python/04_typecasting/example.py
@@ -0,0 +1,17 @@
+a = 45
+b = "56"
+c = 3.14
+d = "89"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+e = int(d)
+print(e)
+print(type(e))
