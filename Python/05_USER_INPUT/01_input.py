95130f38d27533ec1c7ef34461a0e6679605660c
Zakir Hussain Parrey <zakirhussainparrey@gmail.com>
2025-08-05 20:18:45 +0530

    Add user input and addition Python scripts
    Added /05_USER_INPUT/01_input.py for basic user input operations.
    
    Added /05_USER_INPUT/02_add.py implementing an addition function.
    
    Both scripts are part of the user input learning module for Python.

---
+++ Python/05_USER_INPUT/01_input.py
@@ -0,0 +1,5 @@
+a = input("Enter a number ")
+print(a)
+
+b = input("Enter your name: ")
+print(b)
---
+++ Python/05_USER_INPUT/02_add.py
@@ -0,0 +1,12 @@
+# a = input("Enter a first number: ")
+# print(a)
+
+# b = input("Enter a second number: ")
+# print(b)
+# a = int(a) # Convert input to integer
+# b = int(b) # Convert input to integer
+
+a = int(input("Enter first number: "))   # Convert input to integer
+# This line is added to convert the input to an integer
+b = int(input("Enter second number: "))  # Convert input to integer
+print(a + b)
